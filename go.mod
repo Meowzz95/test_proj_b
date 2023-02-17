fcc30c3ce14d3b04fe5471b33319eb82160b964f
meowzz95 <22430379+Meowzz95@users.noreply.github.com>
2023-02-17 15:12:25 +0800

    add dep C

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/meowzz95/test_proj_b
 
 go 1.19
+
+require github.com/meowzz95/test_proj_c v0.0.0-20230217071011-ac92989c4c2f // indirect
--- model.go
+++ model.go
@@ -1,5 +1,8 @@
 package test_proj_b
 
+import "github.com/meowzz95/test_proj_c"
+
 type B struct {
-	Field1 string
+	Field1  string
+	StructC test_proj_c.C
 }
